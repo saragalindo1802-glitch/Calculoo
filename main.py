0a19abae26834935a6febb6a1f00fb9dea106abe
saragalindo1802-glitch <saragalindo1802@gmail.com>
2026-05-28 17:05:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 
+from src.figuras import crear_tricilindro
+from src.volumenes import calcular_volumen, graficar_figura
+
+# =========================
+# Parámetros
+# =========================
+
+radio = 2
+n = 80
+
+# =========================
+# Crear figura
+# =========================
+
+X, Y, Z, figura = crear_tricilindro(radio, n)
+
+# =========================
+# Calcular volumen
+# =========================
+
+volumen = calcular_volumen(figura, radio, n)
+
+print(f"\nVolumen aproximado: {volumen:.4f}")
+
+# Volumen teórico
+volumen_teorico = 8 * (2 - 2**0.5) * radio**3
+
+print(f"Volumen teórico: {volumen_teorico:.4f}")
+
+# Error
+error = abs(volumen - volumen_teorico)
+
+print(f"Error: {error:.4f}")
+
+# =========================
+# Graficar
+# =========================
+
+graficar_figura(
+    X,
+    Y,
+    Z,
+    figura,
+    f"Tricilindro de radio {radio}"
+)
