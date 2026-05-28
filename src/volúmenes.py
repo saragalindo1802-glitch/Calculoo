1e55c8780a2c61bed2ff23bceb3c6f3da468da51
saragalindo1802-glitch <saragalindo1802@gmail.com>
2026-05-28 17:10:43 -0500

    Update volúmenes.py

--- src/volúmenes.py
+++ src/volúmenes.py
@@ -1,1 +1,55 @@
+import numpy as np
+import plotly.graph_objects as go
 
+def calcular_volumen(figura, radio, n):
+
+    limite = radio
+
+    dx = (2 * limite) / n
+
+    volumen_elemento = dx**3
+
+    volumen = np.sum(figura) * volumen_elemento
+
+    return volumen
+
+
+def graficar_figura(X, Y, Z, figura, titulo):
+
+    fig = go.Figure(data=go.Isosurface(
+
+        x=X.flatten(),
+        y=Y.flatten(),
+        z=Z.flatten(),
+
+        value=figura.flatten(),
+
+        isomin=0.5,
+        isomax=1.0,
+
+        surface_count=1,
+
+        colorscale='Viridis',
+
+        showscale=False,
+
+        caps=dict(
+            x_show=False,
+            y_show=False,
+            z_show=False
+        )
+    ))
+
+    fig.update_layout(
+
+        title=titulo,
+
+        scene=dict(
+            xaxis_title='X',
+            yaxis_title='Y',
+            zaxis_title='Z',
+            aspectmode='cube'
+        )
+    )
+
+    fig.show()
